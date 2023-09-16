5ed6ecc77eb34104300dacc7d793c57c6473e63c
Vu Tam R <vutam311291@gmail.com>
2023-09-16 11:33:27 +0700

    ExpressJS with npm

--- server.js
+++ server.js
@@ -1,14 +1,30 @@
-const http = require('http'); // library node.js
+/*-- Node JS -- */
+// const http = require('http'); // library node.js
 
-const hostname = '127.0.0.1'; // localhost
-const port = 3000; // port 0 -> 65535 (max)
+// const hostname = '127.0.0.1'; // localhost
+// const port = 3000; // port 0 -> 65535 (max)
 
-const server = http.createServer((req, res) => {
-	res.statusCode = 200;
-	res.setHeader('Content-Type', 'text/plain');
-	res.end('Hello World\n My name is Jen');
+// const server = http.createServer((req, res) => {
+// 	res.statusCode = 200;
+// 	res.setHeader('Content-Type', 'text/plain');
+// 	res.end('Hello World\n My name is Jen');
+// });
+
+// server.listen(port, hostname, () => {
+// 	console.log(`Server running at http://${hostname}:${port}/`);
+// });
+
+/* -- Express JS -- */
+const express = require('express'); // library express.js
+// import express from 'express'; // Library express.js'
+
+const app = express(); // instance express.js
+const port = 3000; // port 3000
+
+app.get('/', (req, res) => {
+	res.send('Hello Express');
 });
 
-server.listen(port, hostname, () => {
-	console.log(`Server running at http://${hostname}:${port}/`);
+app.listen(port, () => {
+	console.log(`Example app is listening on port ${port}`);
 });
